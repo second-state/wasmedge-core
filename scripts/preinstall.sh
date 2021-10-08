0aa29b6c0ab890c71d4ef7af49da5be12ccf5108
dm4 <dm4@secondstate.io>
2021-10-08 11:26:50 +0800

    [Addon] fix WasmEdge AOT malloc issue

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -2,7 +2,7 @@
 
 INSTALL_SCRIPT=/tmp/install_wasmedge.sh
 INSTALL_PATH="$HOME/.wasmedge"
-INSTALL_VERSION=0.8.1
+INSTALL_VERSION=0.8.2
 SUDO=
 
 wget -O "$INSTALL_SCRIPT" "https://raw.githubusercontent.com/WasmEdge/WasmEdge/766654ee0b655a06156278854d22be8042a2c6bd/utils/install.sh"
--- src/wasmedgeaddon.cc
+++ src/wasmedgeaddon.cc
@@ -128,7 +128,6 @@ void WasmEdgeAddon::InitVM(const Napi::CallbackInfo &Info) {
     return;
   }
 
-  Store = WasmEdge_StoreCreate();
   Configure = WasmEdge_ConfigureCreate();
   Stat = WasmEdge_StatisticsCreate();
   WasmEdge_ConfigureAddProposal(Configure,
@@ -139,7 +138,7 @@ void WasmEdgeAddon::InitVM(const Napi::CallbackInfo &Info) {
                                         WasmEdge_HostRegistration_Wasi);
   WasmEdge_ConfigureAddHostRegistration(
       Configure, WasmEdge_HostRegistration_WasmEdge_Process);
-  VM = WasmEdge_VMCreate(Configure, Store);
+  VM = WasmEdge_VMCreate(Configure, nullptr);
 
   WasmEdge_LogSetErrorLevel();
 
@@ -165,7 +164,6 @@ void WasmEdgeAddon::FiniVM() {
   Stat = WasmEdge_VMGetStatisticsContext(VM);
   WasmEdge_VMDelete(VM);
   VM = nullptr;
-  WasmEdge_StoreDelete(Store);
   Store = nullptr;
   WasmEdge_ConfigureDelete(Configure);
   Configure = nullptr;
@@ -669,6 +667,7 @@ void WasmEdgeAddon::LoadWasm(const Napi::CallbackInfo &Info) {
   }
 
   // Get memory instance
+  Store = WasmEdge_VMGetStoreContext(VM);
   uint32_t MemLen = WasmEdge_StoreListMemoryLength(Store);
   WasmEdge_String MemNames[MemLen];
   WasmEdge_StoreListMemory(Store, MemNames, MemLen);
