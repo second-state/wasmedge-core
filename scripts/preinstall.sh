4df146268f68ea8b19a362b9c8f43a5ffcf765b4
dm4 <dm4@secondstate.io>
2021-10-20 11:30:47 +0800

    [Script] Use install script from master branch of WasmEdge

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-INSTALL_SCRIPT=/tmp/install_wasmedge.sh
+set -e
+
+INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.sh"
+UNINSTALL_SCRIPT_URL="https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/uninstall.sh"
+INSTALL_SCRIPT_PATH=/tmp/install_wasmedge.sh
+UNINSTALL_SCRIPT_PATH=/tmp/uninstall_wasmedge.sh
 INSTALL_PATH="$HOME/.wasmedge"
 INSTALL_VERSION=0.8.2
 SUDO=
-
-wget -O "$INSTALL_SCRIPT" "https://raw.githubusercontent.com/WasmEdge/WasmEdge/766654ee0b655a06156278854d22be8042a2c6bd/utils/install.sh"
 [ "$GITHUB_ACTIONS" == true ] && SUDO=sudo
-$SUDO bash "$INSTALL_SCRIPT" -p "$INSTALL_PATH" --version "$INSTALL_VERSION"
+
+mkdir -p "$INSTALL_PATH"
+
+wget -O "$UNINSTALL_SCRIPT_PATH" "$UNINSTALL_SCRIPT_URL"
+$SUDO bash "$UNINSTALL_SCRIPT_PATH" -p "$INSTALL_PATH" -q
+
+wget -O "$INSTALL_SCRIPT_PATH" "$INSTALL_SCRIPT_URL"
+$SUDO bash "$INSTALL_SCRIPT_PATH" -p "$INSTALL_PATH" -v "$INSTALL_VERSION"
