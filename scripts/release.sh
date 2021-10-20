b82ebf3f215b5b81ae4556f3164e1d45631f2cda
dm4 <dm4@secondstate.io>
2021-10-20 11:30:47 +0800

    [Script] Use install script from master branch of WasmEdge

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+set -e
+
 ARCH=$(node -e 'console.log(process.arch)')
+MODULE_NAME=wasmedge
 npm run release
-rm -rf wasmedge
-mkdir wasmedge
-cp build/Release/wasmedge.node wasmedge
-strip wasmedge/wasmedge.node
-tar zcvf wasmedge-linux-${ARCH}.tar.gz wasmedge
+rm -rf $MODULE_NAME
+mkdir $MODULE_NAME
+cp build/Release/$MODULE_NAME.node $MODULE_NAME
+strip $MODULE_NAME/$MODULE_NAME.node
+tar zcvf $MODULE_NAME-linux-${ARCH}.tar.gz $MODULE_NAME
